8edda3b746cd3db45cf56c0e540bbc2eb6baaa3b
Crihstito <55474637+Crihstito@users.noreply.github.com>
2019-09-25 13:32:35 -0500

    Delete Lluvias.py

--- Lluvias.py
+++
@@ -1,7 +0,0 @@
-import xlrd
-for anio in range(1985,2020):
-    path = "./Precipitacion/"+str(anio)+"Precip.xls"
-    #print(path)
-    archivo = xlrd.open_workbook(filename=path)
-    hoja=archivo.sheet_by_index(0)
-    print(hoja.cell_value(2,1))
