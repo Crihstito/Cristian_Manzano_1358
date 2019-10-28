e53fb4d7c50e46fe6d8575dfda8438058ede6ec1
Crihstito <55474637+Crihstito@users.noreply.github.com>
2019-10-28 12:33:27 -0600

    Update Listadoblementeenlazada.py

--- Listadoblementeenlazada.py
+++ Listadoblementeenlazada.py
@@ -1,40 +1,92 @@
-class NodoDoble:
-    def __init__(self,value,siguiente,previo):
-        self.data = value
-        self.next = siguiente
-        self.prev = previo
-    
-class ListaDoblementeEnlazada:
-    def __init__(self):
-        self.__head = NodoDoble(None,None,None)
-        self.__tail = NodoDoble(None,None,None)
-        self.__head.next = self.__tail
-        self.__tail.prev = self.__head
-        self.__size = 0
+class NodoDoble():
+    def __init__(self,dato):
+    self.dato = dato
+    self.siguiente = None
+    self.anterior = None
 
+class listasDoblementeEnlazadas():
+
+    def __init__ (self):
+        self.size = 0
+        self.primero= None
+        self.ultimo= None
+        
     def get_size(self):
-        return self.__size
-    def insert(self,value):
-        if self.__size == 0:
-            nuevo = NodoDoble(value,self.__tail,self.__head)
-            self.__head.next = nuevo
-            self.__tail.prev = nuevo
+        return self.size
+        
+    def is_empty(self):
+        return self.primero == None
+
+    def insert( self ,value ): 
+        if self.vacia():
+            self.primero = self.ultimo = NodoDoble(dato)
         else:
-            nuevo = NodoDoble(value,self.__tail,self.__tail.prev)
-            self.__tail.prev.next = nuevo
-            self.__tail.prev = nuevo
-        self.__size += 1
-
+            aux = self.ultimo
+            self.ultimo = aux.siguiente = NodoDoble(dato)
+            self.ultimo.anterior = aux
+        self.size += 1
+            
+    def find_from_tail(self, value ):
+        aux = self.primero
+        while aux:
+            if aux.dato == dato:
+                return True
+            else:
+                aux = aux.siguiente
+                if aux == self.primero:
+                    return False
+        
+    def find_from_head(self, value ):
+        aux = self.ultimo
+        while aux:
+            if aux.dato == dato:
+                return True
+            else:
+                aux = aux.anterior
+                if aux == self.ultimo:
+                    return False
+    
+    def remove_from_head(self):
+        if self.vacia():
+            print ("Esta vacia")
+        elif self.primero.siguiente == None:
+            self.primero=self.ultimo=None
+            self.size=0
+        else:
+            self.primero=self.primero.siguiente
+            self.primero.anterior = None
+            self.size -=1
+            
+    def remove_from_tail( value ):
+        if self.vacia():
+            print ("Esta vacia")
+        elif self.primero.siguiente == None:
+            self.primero = self.ultimo = None
+            self.size = 0
+        else:
+            self.ultimo = self.ultimo.anterior
+            self.ultimo.siguiente = None
+            self.size -=1
+    
+    def insert_between(  value , predecesor ,sucesor):
+        if self.size == 0:
+            nuevo = NodoDoble(value, self.ultimo, self.primero)
+            self.primero.siguiente=nuevo
+            self.ultimo.anterior=nuevo
+        else:
+            nuevo = NodoDoble(value, self.ultimo, self.ultimo.anterior)
+            self.ultimo.anterior.siguiente=nuevo
+            self.ultimo.anterior=nuevo
+        self.size += 1
+            
     def transversal(self):
-        curr_Node = self.__head.next
-        while curr_Node.next != None:
-            print(curr_Node.data,"->",end=" ")
-            curr_Node = curr_Node.next
-        print("")
-
+        aux = self.primero
+        while aux:
+            print(aux.dato)
+            aux=aux.siguiente
+        
     def reverse_transversal(self):
-        curr_Node = self.__tail.prev
-        while curr_Node.prev != None:
-            print(curr_Node.data,"->",end=" ")
-            curr_Node = curr_Node.prev
-        print("")
+        aux = self.primero
+        while aux:
+            print(aux.dato)
+            aux=aux.anterior
