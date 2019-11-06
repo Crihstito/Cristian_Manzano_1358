38aa7a0f915babe064345b46a3f95bb8d2dbd0e3
Crihstito <55474637+Crihstito@users.noreply.github.com>
2019-11-05 23:02:16 -0600

    Create Función_suma_lista.py

---
+++ Función_suma_lista.py
@@ -0,0 +1,3 @@
+# Una función recursiva que calcule la suma de los elementos de una lista que al ser llamada de la siguiente forma imprima 11. 
+lst = [3,5,2,1]
+print( suma_lista(lst) ) # debe imprimir 11
